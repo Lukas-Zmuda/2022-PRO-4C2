17f617f88a9f8b7c34b5dbc7523b1ae7b2f59a6a
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-20 12:56:38 +0200

    prvy

---
+++ PrvyProjekt/src/MojaPrva.java
@@ -0,0 +1,16 @@
+
+public class MojaPrva {
+	public static void main(String[] args) {
+		System.out.println("Lukas");
+		
+		System.out.println(10);
+		System.out.println(10 + 5);
+		
+		System.out.println("10 + " + 5);
+		
+		System.out.println("10 + 5 = " + ( 10 + 5));
+	}
+
+	
+
+}
