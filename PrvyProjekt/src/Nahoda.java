0371b5466d88bfb5c1710e0d2c152b1298a92de9
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-01-12 12:46:59 +0100

    Create Nahoda.java

---
+++ PrvyProjekt/src/Nahoda.java
@@ -0,0 +1,14 @@
+
+public class Nahoda {
+
+	public static void main(String[] args) {
+		
+		double nahodne = Math.random();
+		System.out.println(nahodne);
+		
+		int n = (int)(Math.random() * 100) + 100;
+		System.out.println(n);
+
+	}
+
+}
